7a92d3878bd7734a74f653e6ee8e4e5efd527d13
Eduardo Lujan <eduardolujan@eduardos-air.lan>
2018-04-23 22:25:33 -0500

    Initial Commit

---
+++ finalProject30/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalProject30
+//
+//  Created by Eduardo Lujan on 23/04/18.
+//  Copyright © 2018 Eduardo Lujan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
