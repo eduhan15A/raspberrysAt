85b25d07f97dde37430c3d7884bceb4907cf621f
Eduardo Lujan <eduardolujan@eduardos-air.lan>
2018-04-25 02:23:55 -0500

    Se agrega servicio web para mostrar los dispositivos y temperaturas en el mapa. Se muestra la ubicación del usuario.

---
+++ Artwork.swift
@@ -0,0 +1,30 @@
+//
+//  Artwork.swift
+//  finalProject30
+//
+//  Created by Eduardo Lujan on 25/04/18.
+//  Copyright © 2018 Eduardo Lujan. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+}
--- finalProject30/ViewController.swift
+++ finalProject30/ViewController.swift
@@ -8,19 +8,40 @@
 
 import UIKit
 import MapKit
-class ViewController: UIViewController {
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
     
+    var ids = [String]()
+    var longitudes = [String]()
+    var latitudes = [String]()
+    var elevations = [String]()
+    var dates = [String]()
+    var temperatures = [String]()
+    var devices = [String]()
+    var locationManager: CLLocationManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        map.showsUserLocation = true
+        map.isZoomEnabled = true
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
         
-        let url = URL(string: "https://free.currencyconverterapi.com/api/v5/countries")!
+        map.delegate = self
+        
+        let url = URL(string: "http://www.itesi.xyz/webservices/damAPI/")!
         var request = URLRequest(url: url)
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "GET"
-        //let postString = "id=13&name=Jack"
-       // request.httpBody = postString.data(using: .utf8)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let postString = "{\"action\":\"GetTemperature\"}";
+       request.httpBody = postString.data(using: .utf8)
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {                                                 // check for fundamental networking error
                 print("error=\(error)")
@@ -41,9 +62,64 @@ class ViewController: UIViewController {
 
     func printDots(response:String)-> Bool{
         print("responseString = \(response)")
+        do {
+            let data = response.data(using: .utf8)
+            if let data = data,
+                let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                let blogs = json["Data"] as? [[String: Any]] {
+                for blog in blogs {
+                    if let name = blog["idMedition"] as? String {
+                        ids.append(name)
+                    }
+                    if let name = blog["length"] as? String {
+                        longitudes.append(name)
+                    }
+                    if let name = blog["latitude"] as? String {
+                        latitudes.append(name)
+                    }
+                    if let name = blog["elevation"] as? String {
+                        elevations.append(name)
+                    }
+                    if let name = blog["datetime"] as? String {
+                        dates.append(name)
+                    }
+                    if let name = blog["temperature"] as? String {
+                        temperatures.append(name)
+                    }
+                    if let name = blog["device"] as? String {
+                        devices.append(name)
+                    }
+                }
+            }
+        } catch {
+            print("Error deserializing JSON: \(error)")
+        }
         
+      //  print(devices)
+       // print(temperatures)
+        
+        for i in 0 ... ids.count-1{
+            print(devices[i])
+            print(temperatures[i])
+            
+            let artwork = Artwork(title: devices[i],
+                                  locationName: temperatures[i]+"°C "+dates[i],
+                                  discipline: "",
+                                  coordinate: CLLocationCoordinate2D(latitude: Double(latitudes[i])!, longitude: Double(longitudes[i])!))
+            map.addAnnotation(artwork)
+        }
         
         return true
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.075, longitudeDelta: 0.075))
+        
+        self.map.setRegion(region, animated: true)
+    
     }
     
     override func didReceiveMemoryWarning() {
@@ -53,4 +129,29 @@ class ViewController: UIViewController {
 
 
 }
+
+extension ViewController: MKMapViewDelegate {
+    // 1
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // 2
+        guard let annotation = annotation as? Artwork else { return nil }
+        // 3
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        // 4
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            // 5
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+}
+
 
